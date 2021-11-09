25415629adf51be2ff6197bb8631f68fb4dafb81
WalterFrongia <walter.frongia@libero.it>
2021-11-09 16:13:23 +0100

    Implementati dati del model in show.blade e dati un layout con show scss

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,6 +2,19 @@
 
 @section('comics')
 
+    <div id="show-wrapper">
 
+        <div id="comic-container">
+            <div id="img-container">
+                <img src="{{$comic->url_img}}" alt="">
+            </div>
+            <div id="info-container">
+                <h3>{{$comic->title}}</h3>
+                <p>DESCRIPTION: {{$comic->description}}</p>
+                <p>PRICE: {{$comic->price}}</p>
+                <p>DATE: {{$comic->sale_date}}</p>
+            </div>
+        </div>
+    </div>
 
 @endsection
