bc4b499f2150e83cdb8104a351d13e59f324edaa
WalterFrongia <walter.frongia@libero.it>
2021-11-10 16:15:57 +0100

    Implementata pagina create con gli input

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.main')
 
-@section('content')
+@section('comics')
 
+    <form action="">
+
+        <input type="text" placeholder="inserisci il titolo">
+
+        <input type="text" placeholder="inserisci una descrizione">
+
+        <input type="text" placeholder="inserisci link url. img.">
+
+        <input type="text" placeholder="inserisci il prezzo">
+
+        <input type="text" placeholder="inserisci data di uscita">
+
+    </form>
 
 @endsection
